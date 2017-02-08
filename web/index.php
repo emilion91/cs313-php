59a0c9b086aeb498d3fbce40f1e825d442b77940
cesar de leon <deleon.cesar08@gmail.com>
2017-02-07 22:04:03 -0700

    index.php

--- web/index.php
+++ web/index.php
@@ -20,6 +20,7 @@
 			<li><a class="bold" href="assignments.html">Assigments</a></li>
 			<li><a class="bold" href="tassign03.php">Team Assigment 03</a></li>
       <li><a class="bold" href="week5/assign.php">Week 05 team assign</a></li>
+      <li><a class="bold" href="tAct6/assign.php">Week 06 team assign</a></li>
       <li><a class="bold" href="project/login.php">Warehouse Manager</a></li>
 
 		</ul>
