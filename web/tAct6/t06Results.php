c3e88398e4dd9b164ddc17cc4b59fd2e2629b9ae
cesar de leon <deleon.cesar08@gmail.com>
2017-02-08 21:41:22 -0700

    part 2

--- web/tAct6/t06Results.php
+++ web/tAct6/t06Results.php
@@ -3,15 +3,14 @@
     <head>
 <?php
 
-$$db = pg_connect('host=ec2-23-21-238-246.compute-1.amazonaws.com dbname=dbfuplouemrf5 user=itenvwthrjxvzg
- password=fe4517ed2fd76bc283b5c66571a0fb4de965b489b47b2481a8e19bba1a7e399c');
+$db = new PDO('pgsql:host=ec2-23-21-238-246.compute-1.amazonaws.com;port=5432;dbname=dbfuplouemrf5,itenvwthrjxvzg,fe4517ed2fd76bc283b5c66571a0fb4de965b489b47b2481a8e19bba1a7e399c');
 
 $book = $_POST['book'];
 $chapter = $_POST['chapter'];
 $verse = $_POST['verse'];
 $content = $_POST['content'];
 
-$query = 'INSERT INTO scripture(book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)';
+$query = 'INSERT INTO scripture(book, chapter, verse, content) VALUES( 7, :book, :chapter, :verse, :content)';
 $statement = $db->prepare($query);
 
 $statement->bindValue(':book', $book);
