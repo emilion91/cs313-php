c7698b9d36110429944af27099d27eed938d7f76
cesar de leon <deleon.cesar08@gmail.com>
2017-02-08 21:31:51 -0700

    part 2

---
+++ web/tAct6/t06Results.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+<?php
+
+$$db = pg_connect('host=ec2-23-21-238-246.compute-1.amazonaws.com dbname=dbfuplouemrf5 user=itenvwthrjxvzg
+ password=fe4517ed2fd76bc283b5c66571a0fb4de965b489b47b2481a8e19bba1a7e399c');
+
+$book = $_POST['book'];
+$chapter = $_POST['chapter'];
+$verse = $_POST['verse'];
+$content = $_POST['content'];
+
+$query = 'INSERT INTO scripture(book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)';
+$statement = $db->prepare($query);
+
+$statement->bindValue(':book', $book);
+$statement->bindValue(':chapter', $chapter);
+$statement->bindValue(':verse', $verse);
+$statement->bindValue(':content', $content);
+
+$statement->execute();
+?>
+    </head>
+    <body>
+    </body>
+</html>
