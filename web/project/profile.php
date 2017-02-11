e633e59420fb481519256e183a970a486af82142
cesar de leon <deleon.cesar08@gmail.com>
2017-02-11 16:32:11 -0700

    profile fix

--- web/project/profile.php
+++ web/project/profile.php
@@ -5,45 +5,31 @@ session_start();
 <html>
 
 <head>
-        <?php
-
-        switch ($_SESSION['clearence']){
-            case "1";
-                echo "General Manager";
-                break;
-            case "2";
-                echo "Manager";
-                break;
-            default;
-                echo "Employee";
-
-        ?>
-            <title> </title>
-<link rel="stylesheet" type="text/css" href="basicstyle.css">
-    </head>
-    <body>
-	<div class="container">
-	<header>
-	<
-	</header>
-
-	<nav>
-    <ul>
-    <li><a class="bold" href="aboutus.php">About Us</a></li>
-    <li><a class="bold" href="otheroption.php">Other Options</a></li>
-    </ul>
-	</nav>
+    <title>Warehouse Manager</title>
+    <link rel="stylesheet" type="text/css" href="basicstyle.css">
+</head>
+<body>
+	  <div class="container">
+	      <header>
 	
-	<article>
-      <?php
-      echo "Welcome back " . $_SESSION['firstn'] . " " . $_SESSION['lastn'] . "<br />";
-      ?>
-	
-	</article>
+	      </header>
 
-	<footer>
-		
-	</footer>
-	</div>
-    </body> 
-    </html> 
+	      <nav>
+            <ul>
+                <li><a class="bold" href="aboutus.php">About Us</a></li>
+                <li><a class="bold" href="otheroption.php">Other Options</a></li>
+            </ul>
+	      </nav>
+	      
+	      <article>
+            <?php
+            echo "Welcome back " . $_SESSION['firstn'] . " " . $_SESSION['lastn'] . "<br />";
+            ?>
+	          
+	      </article>
+        
+	      <footer>
+	      </footer>
+	  </div>
+</body>
+</html>
