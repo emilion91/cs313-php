8a0d3cec54402a7fb8460d73f2014e0e3c866c90
cesar de leon <deleon.cesar08@gmail.com>
2017-02-14 16:22:13 -0700

    sign in update

--- web/project/signin.php
+++ web/project/signin.php
@@ -3,7 +3,7 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
-    
+
     <head>
         <title>Sign Up</title>
         <link rel="stylesheet" type="text/css" href="basicstyle.css">
@@ -13,21 +13,21 @@ session_start();
         <?php
         $db = pg_connect('host=ec2-23-21-238-246.compute-1.amazonaws.com dbname=dbfuplouemrf5 user=itenvwthrjxvzg
  password=fe4517ed2fd76bc283b5c66571a0fb4de965b489b47b2481a8e19bba1a7e399c');
-        
+
         $query="SELECT * FROM warehouses";
-        
+
         $result = pg_query($query);
         if (!$result) {
             echo "Problem with query " . $query . "<br/>";
             echo pg_last_error();
             exit();
         }
-        
+
         ?>
 	      <div class="container">
 	          <header>
 	          </header>
-            
+
 	          <nav>
                 <ul>
                     <li><a class="bold" href="aboutus.php">About Us</a></li>
@@ -35,7 +35,7 @@ session_start();
                     <li><a class="bold" href="login.php">Log In</a></li>
                 </ul>
 	          </nav>
-            
+
 	          <article>
                 <h1>Sign Up</h1>
                 <h2>Your Information<h2>
@@ -46,7 +46,7 @@ session_start();
                             <label>Last Name:</label><br />
                             <input id="lastn" name="lastn" type="text" required="required" /><br />
                             <label>Email:</label><br />
-                            <input id="email" name="email" type="text" required="required" /><br />
+                            <input id="email" name="email" type="text" required="required" onblur="validate()"/><br />
                             <label>Password:</label><br />
                             <input id="password" name="password" type="password" /><br />
                             <label>Confirm Passwork:</label><br />
