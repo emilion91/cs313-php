b8a1b9194d965a0174f0088fcab58f59cf690987
cesar de leon <deleon.cesar08@gmail.com>
2017-02-17 16:15:29 -0700

    account update update

--- web/project/accountupdate.php
+++ web/project/accountupdate.php
@@ -14,7 +14,7 @@ $db = pg_connect('host=ec2-23-21-238-246.compute-1.amazonaws.com dbname=dbfuplou
  password=fe4517ed2fd76bc283b5c66571a0fb4de965b489b47b2481a8e19bba1a7e399c');
 
     $clearence = $_POST['clearence'];
-    $employeeid = $_SESSION['employee'];
+    $employeeid = $_SESSION['employeeid'];
     $query = "UPDATE employees SET CLEARENCE = '$clearence' WHERE employeeid = '$employeeid'";
 
 
