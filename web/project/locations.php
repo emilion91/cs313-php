738f62d6fcf8c3d312b43d6080273fecb2270bb2
cesar de leon <deleon.cesar08@gmail.com>
2017-02-17 13:43:19 -0700

    new warehouse

--- web/project/locations.php
+++ web/project/locations.php
@@ -21,7 +21,7 @@ $db = pg_connect('host=ec2-23-21-238-246.compute-1.amazonaws.com dbname=dbfuplou
 
 	<nav>
     <ul>
-        <li><a class="bold" href="profile">Home</a></li>
+        <li><a class="bold" href="profile.php">Home</a></li>
         <li><a class="bold" href="aboutus.php">About Us</a></li>
         <li><a class="bold" href="alerts.php">Profile Alerts</a></li>
     </ul>
@@ -42,8 +42,11 @@ $db = pg_connect('host=ec2-23-21-238-246.compute-1.amazonaws.com dbname=dbfuplou
         if ($myrow == FALSE){
             echo "<h2 class=\"alert\" >There are no Warehouses in the Database</h2>";
         }else{
-            while($row = pg_fetch_assoc($result)) {
+            printf ("<strong>%s %s: %s - </strong> \"%s\" <br>",htmlspecialchars($myrow['whname']), htmlspecialchars($myrow['whaddress']));
+
+            while($myrow = pg_fetch_assoc($result)) {
 				        printf ("<strong>%s %s: %s - </strong> \"%s\" <br>",htmlspecialchars($myrow['whname']), htmlspecialchars($myrow['whaddress']));
+
             }
         }
 
