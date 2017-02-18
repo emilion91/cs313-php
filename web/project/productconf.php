9bdf969b0514c4074ac341921697423fa25d4058
cesar de leon <deleon.cesar08@gmail.com>
2017-02-18 15:16:45 -0700

    new products

--- web/project/productconf.php
+++ web/project/productconf.php
@@ -13,7 +13,7 @@ session_start();
 $db = pg_connect('host=ec2-23-21-238-246.compute-1.amazonaws.com dbname=dbfuplouemrf5 user=itenvwthrjxvzg
  password=fe4517ed2fd76bc283b5c66571a0fb4de965b489b47b2481a8e19bba1a7e399c');
 
-$pname = str_repalce(' ', '_', $_POST['pname']);
+$pname = str_replace(' ', '_', $_POST['pname']);
 $price = $_POST['price'];
 
 $query = "INSERT INTO products (productname, price) VALUES ('$pname', '$price')";
