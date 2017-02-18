031e131c9454cbaaba3413ce3539c565b59624cc
cesar de leon <deleon.cesar08@gmail.com>
2017-02-18 14:22:35 -0700

    accountupdate

--- web/project/accountupdate.php
+++ web/project/accountupdate.php
@@ -14,26 +14,51 @@ $db = pg_connect('host=ec2-23-21-238-246.compute-1.amazonaws.com dbname=dbfuplou
  password=fe4517ed2fd76bc283b5c66571a0fb4de965b489b47b2481a8e19bba1a7e399c');
 
     $clearence = $_POST['clearence'];
+    $warehouse = $_POST['warehouse'];
     $employeeid = $_SESSION['employeeid'];
-    $query = "UPDATE employees SET CLEARENCE = '$clearence' WHERE employeeid = '$employeeid'";
 
-
-
-    if ($_POST['warehouse'] != NULL){
-        $whid = $_POST['warehouse'];
-        $query = "UPDATE employees SET clearence = '$clearence', whid = '$whid' WHERE employeeid = '$employeeid'";
-    } 
-
-    $result = pg_query($query);
-        if (!$result) {
-            echo "Problem with query " . $query . "<br/>";
-            echo pg_last_error();
-            exit();
-        }
-
+if ($warehouse != NULL && $clearence != NULL){
+    $whid = $_POST['warehouse'];
+    $query = "UPDATE employees SET clearence = '$clearence', whid = '$whid' WHERE employeeid = '$employeeid'";
     $_SESSION['clearence'] = $clearence;
+    $result = pg_query($query);
+    if (!$result) {
+        echo "Problem with query " . $query . "<br/>";
+        echo pg_last_error();
+        exit();
+    }
+
+} 
+
+if ($warehouse == NULL && $clearence != NULL){
+    $whid = $_POST['warehouse'];
+    $query = "UPDATE employees SET clearence = '$clearence' WHERE employeeid = '$employeeid'";
+    $_SESSION['clearence'] = $clearence;
+    $result = pg_query($query);
+    if (!$result) {
+        echo "Problem with query " . $query . "<br/>";
+        echo pg_last_error();
+        exit();
+    }
+
+} 
+
+if ($warehouse != NULL && $clearence == NULL){
+    $whid = $_POST['warehouse'];
+    $query = "UPDATE employees SET whid = '$whid' WHERE employeeid = '$employeeid'";
+    $result = pg_query($query);
+    if (!$result) {
+        echo "Problem with query " . $query . "<br/>";
+        echo pg_last_error();
+        exit();
+    }
+
+} 
 
 
+    if ($_POST['warehouse'] == NULL && $clearence == NULL){
+        echo "<p>Nothing to update on your profile</p><br />";
+    }
 
 ?>
 	  <div class="container">
@@ -45,6 +70,15 @@ $db = pg_connect('host=ec2-23-21-238-246.compute-1.amazonaws.com dbname=dbfuplou
             <ul>
                 <li><a class="bold" href="profile.php">Home</a></li>
 	      <article>
+                            <?php
+                            if ($_POST['warehouse'] == NULL && $clearence == NULL){
+                                echo "<p>Nothing to update on your profile</p><br />";
+                            }
+                            else{
+                                echo "<p>Your profile has been updated</p>";
+                            }
+?>
+
 	      </article>
 
 	      <footer>
